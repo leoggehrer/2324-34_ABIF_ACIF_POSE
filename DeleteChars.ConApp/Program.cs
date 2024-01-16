e501c0acbc1cac05681f498e38a258c0ecc55284
Gehrer Gerhard <56598504+leoggehrer@users.noreply.github.com>
2024-01-16 15:20:51 +0100

    Added DeleteChars and StringSearch

---
+++ DeleteChars.ConApp/Program.cs
@@ -0,0 +1,60 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 DeleteChars
+*                 Extracts specified characters from any text.
+*----------------------------------------------------------
+*/
+
+#nullable disable
+namespace DeleteChars.ConApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText, eliminatorText, outputText;
+
+            Console.WriteLine("Delete Chars!");
+            Console.WriteLine("=============");
+
+            // Input (I)
+            outputText = string.Empty;
+            Console.WriteLine();
+            Console.Write("Eingabetext....: ");
+            inputText = Console.ReadLine();
+            if (inputText != "")
+            {
+                Console.Write("Eliminatortext..: ");
+                eliminatorText = Console.ReadLine();
+
+                // Process (P)
+                for (int i = 0; i < inputText.Length; i++)
+                {
+                    bool contains = false;
+
+                    for (int j = 0; j < eliminatorText.Length && contains == false; j++)
+                    {
+                        if (inputText[i] == eliminatorText[j])
+                        {
+                            contains = true;
+                        }
+                    }
+                    if (contains == false)
+                    {
+                        outputText = outputText + inputText[i];
+                    }
+                }
+
+                // Output (O)
+                Console.WriteLine();
+                Console.WriteLine($"Ausgabetext....: {outputText}");
+
+                Console.Write("Weiter mit beliebiger Taste ...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ StringSearch.ConApp/Program.cs
@@ -0,0 +1,64 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 StringSearch
+*                 Searches for a substring in a character string.
+*----------------------------------------------------------
+*/
+
+#nullable disable
+namespace StringSearch.ConApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText, searchText;
+            int index = -1;
+
+            Console.WriteLine("String Search!");
+            Console.WriteLine("==============");
+
+            // Input (I)
+            Console.WriteLine();
+            Console.Write("Eingabetext....: ");
+            inputText = Console.ReadLine();
+            if (inputText != "")
+            {
+                Console.Write("Suchtext.......: ");
+                searchText = Console.ReadLine();
+
+                // Process (P)
+                for (int i = 0; index == -1 && searchText.Length > 0 && i <= inputText.Length - searchText.Length; i++)
+                {
+                    int j = 0;
+                    bool check = true;
+
+                    while (check && j < searchText.Length)
+                    {
+                        check = char.ToLower(inputText[i + j]) == char.ToLower(searchText[j++]);
+                    }
+
+                    index = check ? i : index;
+                }
+
+                // Output (O)
+                if (index >= 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Der Suchtext wurde an der Position {index} gefunden.");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Der Suchtext wurde nicht gefunden.");
+                }
+
+                Console.Write("Weiter mit Enter...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
