f0d1795a58f44a627302f5159742b522daed4a5b
leoggehrer <g.gehrer@htl-leonding.ac.at>
2024-02-12 12:01:52 +0100

    Added PlantUML (not finished).

---
+++ PlantUML.ConApp/UMLCreator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace PlantUML.ConApp
+{
+    internal class UMLCreator
+    {
+        public static void CreateActivityDiagram(string path, string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+            var syntaxRoot = syntaxTree.GetRoot();
+            var classNodes = syntaxRoot.DescendantNodes().OfType<ClassDeclarationSyntax>();
+
+            foreach (var classNode in classNodes)
+            {
+                var methodNodes = classNode.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+                foreach (var methodNode in methodNodes)
+                {
+                    var fileName = $"{classNode.Identifier.Text}_{methodNode.Identifier.Text}.puml";
+                    var statements = methodNode?.Body?.Statements;
+
+                    foreach(StatementSyntax statement in statements!)
+                    {
+                        if (statement is IfStatementSyntax ifStmt)
+                        {
+
+                        }
+
+                        Console.WriteLine(statement.GetType().Name);
+                        Console.WriteLine($"{statement}");
+                    }
+                }
+            }
+        }
+    }
+}
