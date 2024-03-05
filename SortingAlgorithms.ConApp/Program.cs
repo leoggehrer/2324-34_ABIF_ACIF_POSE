7b7275207fb7cb35939362c6472521ac1bf9cea3
ggehrer <g.gehrer@hotmail.com>
2024-03-05 11:16:30 +0100

    Added diagrams.

---
+++ SortingAlgorithms.ConApp/Program.cs
@@ -0,0 +1,189 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 SortingAlgorithms
+*                 This program implements 3 different sorting 
+*                 algorithms and compares them in terms of 
+*                 performance.
+*                 1. Bubble Sort
+*                 2. Insertion Sort
+*                 3. Selection Sort
+*
+*----------------------------------------------------------
+*/
+#nullable disable
+
+using System.Diagnostics;
+
+namespace SortingAlgorithms.ConApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            const int TEST_SIZE = 20;
+            const int PERFORMANCE_SIZE = 50_000;
+            int[] array;
+
+            Console.WriteLine("Sort-Algorithmen!");
+            Console.WriteLine("=================");
+            Console.WriteLine();
+
+            // Test BruteForceSort
+            Console.WriteLine("Test BruteForceSort");
+            array = CreateRandomArray(TEST_SIZE);
+            PrintArray("Unsorted:", array);
+            Sort.BruteForceSort(array);
+            PrintArray("BruteForceSort:", array);
+            Console.WriteLine();
+
+            // Test BubbleSort
+            Console.WriteLine("Test BubbleSort");
+            array = CreateRandomArray(TEST_SIZE);
+            PrintArray("Unsorted:", array);
+            Sort.BubbleSort(array);
+            PrintArray("BubbleSort:", array);
+            Console.WriteLine();
+
+            // Test InsertionSort
+            Console.WriteLine("Test InsertionSort");
+            array = CreateRandomArray(TEST_SIZE);
+            PrintArray("Unsorted:", array);
+            Sort.InsertionSort(array);
+            PrintArray("InsertionSort:", array);
+            Console.WriteLine();
+
+            // Test SelectionSort
+            Console.WriteLine("Test SelectionSort");
+            array = CreateRandomArray(TEST_SIZE);
+            PrintArray("Unsorted:", array);
+            Sort.SelectionSort(array);
+            PrintArray("SelectionSort:", array);
+            Console.WriteLine();
+
+            // Performance
+            Console.WriteLine("Performance Testing");
+            Console.WriteLine("===================");
+            Console.WriteLine();
+
+            Console.WriteLine("Machine:   " + Environment.MachineName);
+            Console.WriteLine("Version:   " + Environment.Version);
+            Console.WriteLine("Processor: Apple M2");
+            Console.WriteLine("Processor: " + Environment.ProcessorCount + " cores");
+            Console.WriteLine("OS:        " + Environment.OSVersion);
+            Console.WriteLine();
+
+            Stopwatch sw = new Stopwatch();
+            int[] randomArray = CreateRandomArray(PERFORMANCE_SIZE);
+            int[] arrayToSort;
+
+            // Performance BruteForceSort
+            arrayToSort = (int[])randomArray.Clone();
+            Console.WriteLine($"{nameof(Sort.BruteForceSort)}(0...{randomArray.Length})");
+            sw.Start();
+            Sort.BruteForceSort(arrayToSort);
+            sw.Stop();
+            Console.WriteLine($"Zeitmessung: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            // Performance BubbleSort
+            arrayToSort = (int[])randomArray.Clone();
+            Console.WriteLine($"{nameof(Sort.BubbleSort)}(0...{randomArray.Length})");
+            sw.Restart();
+            Sort.BubbleSort(arrayToSort);
+            sw.Stop();
+            Console.WriteLine($"Zeitmessung: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            // Performance InsertionSort
+            arrayToSort = (int[])randomArray.Clone();
+            Console.WriteLine($"{nameof(Sort.InsertionSort)}(0...{randomArray.Length})");
+            sw.Restart();
+            Sort.InsertionSort(arrayToSort);
+            sw.Stop();
+            Console.WriteLine($"Zeitmessung: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            // Performance SelectionSort
+            arrayToSort = (int[])randomArray.Clone();
+            Console.WriteLine($"{nameof(Sort.SelectionSort)}(0...{randomArray.Length})");
+            sw.Restart();
+            Sort.SelectionSort(arrayToSort);
+            sw.Stop();
+            Console.WriteLine($"Zeitmessung: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            // Performance Array.Sort
+            arrayToSort = (int[])randomArray.Clone();
+            Console.WriteLine($"Array.Sort(0...{randomArray.Length})");
+            sw.Restart();
+            Array.Sort(arrayToSort);
+            sw.Stop();
+            Console.WriteLine($"Zeitmessung: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine();
+
+            Console.WriteLine("Exit with ENTER...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Prints the given array with a specified title.
+        /// </summary>
+        /// <param name="title">The title to be displayed before printing the array.</param>
+        /// <param name="array">The array to be printed.</param>
+        private static void PrintArray(string title, int[] array)
+        {
+            Console.WriteLine(title);
+            PrintArray(array);
+        }
+        /// <summary>
+        /// Prints the elements of an integer array.
+        /// </summary>
+        /// <param name="array">The integer array to be printed.</param>
+        private static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > 0)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(array[i]);
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Creates a random array of integers with the specified size.
+        /// </summary>
+        /// <param name="size">The size of the array to create.</param>
+        /// <returns>A random array of integers.</returns>
+        private static int[] CreateRandomArray(int size)
+        {
+            return CreateRandomArray(size, 0, size);
+        }
+        /// <summary>
+        /// Creates an array of random integers within the specified range.
+        /// </summary>
+        /// <param name="size">The size of the array.</param>
+        /// <param name="min">The minimum value (inclusive) of the random integers.</param>
+        /// <param name="max">The maximum value (exclusive) of the random integers.</param>
+        /// <returns>An array of random integers.</returns>
+        private static int[] CreateRandomArray(int size, int min, int max)
+        {
+            int[] result = new int[size];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Random.Shared.Next(min, max);
+            }
+            return result;
+        }
+    }
+}
---
+++ SortingAlgorithms.ConApp/Sort.cs
@@ -0,0 +1,104 @@
+﻿namespace SortingAlgorithms.ConApp
+{
+    public static class Sort
+    {
+        /// <summary>
+        /// Sorts an array using the brute force sorting algorithm.
+        /// </summary>
+        /// <param name="array">The array to be sorted.</param>
+        public static void BruteForceSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sorts an array of integers using the Bubble Sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to be sorted.</param>
+        public static void BubbleSort(int[] array)
+        {
+            bool exchange;
+            int length = array.Length;
+
+            do
+            {
+                exchange = false;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                        exchange = true;
+                    }
+                }
+                length--;
+            } while (exchange);
+        }
+
+        /// <summary>
+        /// Sorts an array of integers using the Insertion Sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to be sorted.</param>
+        public static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                int current = array[i];
+
+                while (j > 0 && array[j - 1] > current)
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = current;
+            }
+        }
+
+        /// <summary>
+        /// Sorts an array of integers using the Selection Sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to be sorted.</param>
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIdx = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+                if (minIdx != i)
+                {
+                    Swap(ref array[i], ref array[minIdx]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Swaps the values of two integers.
+        /// </summary>
+        /// <param name="v1">The first integer.</param>
+        /// <param name="v2">The second integer.</param>
+        public static void Swap(ref int v1, ref int v2)
+        {
+            int tmp = v1;
+
+            v1 = v2;
+            v2 = tmp;
+        }
+    }
+}
