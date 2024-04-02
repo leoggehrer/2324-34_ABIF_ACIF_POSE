c1b6024391cbccc55b199c2555677c60237547f8
ggehrer <g.gehrer@hotmail.com>
2024-04-02 14:28:35 +0200

    Added FileWizard and CommentRate.

---
+++ CommentRate.ConApp/Program.cs
@@ -0,0 +1,142 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*---------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 CommentRate
+*                 This program calculates the ratio between 
+*                 code characters and comment characters.
+*----------------------------------------------------------
+*/
+#nullable disable
+
+namespace CommentRate.ConApp
+{
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main method of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kommentarquote für C#-Dateien");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+
+            string fileName;
+            int codeChars, commentChars;
+            double relationShip;
+
+            fileName = GetFileName();
+            AnalyzeFile(fileName, out codeChars, out commentChars);
+
+            relationShip = (double)commentChars / (codeChars + commentChars) * 100;
+            Console.WriteLine($"Von {codeChars + commentChars} Zeichen waren {commentChars} Kommentare, das ergibt {relationShip:f} % Kommentarquote.");
+            Thread.Sleep(3000);
+        }
+
+        /// <summary>
+        /// Prompts the user to enter a file name and validates if the file exists.
+        /// </summary>
+        /// <returns>The name of the existing file entered by the user.</returns>
+        private static string GetFileName()
+        {
+            int counter = 0;
+            string result = string.Empty;
+            string input;
+
+            do
+            {
+                if (counter++ == 0)
+                {
+                    Console.Write("Dateiname ohne Endung: ");
+                }
+                else
+                {
+                    Console.Write(@"Dateiname ohne Endung <im Anwendungsverzeichnis \bin\debug>: ");
+                }
+                input = $"{Console.ReadLine()}.cs";
+                if (File.Exists(input))
+                {
+                    result = input;
+                }
+            } while (string.IsNullOrEmpty(result));
+            return result;
+        }
+
+        /// <summary>
+        /// Analyzes a file and counts the number of code characters and comment characters.
+        /// </summary>
+        /// <param name="fileName">The path of the file to analyze.</param>
+        /// <param name="codeChars">The number of code characters in the file.</param>
+        /// <param name="commentChars">The number of comment characters in the file.</param>
+        private static void AnalyzeFile(string fileName, out int codeChars, out int commentChars)
+        {
+            int blockCommentStartIdx = -1, blockCommentEndIdx = -1;
+            int lineCommentStartIdx = -1, lineCommentEndIdx = -1;
+            string text = File.ReadAllText(fileName);
+            
+            codeChars = 0;
+            commentChars = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (blockCommentStartIdx == -1 && lineCommentStartIdx == -1)
+                {
+                    blockCommentStartIdx = text.IndexOf("/" + "*", i);
+                    if (blockCommentStartIdx > -1)
+                    {
+                        blockCommentEndIdx = text.IndexOf("*" + "/", blockCommentStartIdx);
+                        if (blockCommentEndIdx > blockCommentStartIdx)
+                        {
+                            blockCommentEndIdx += 2;
+                        }
+                        else
+                        {
+                            blockCommentStartIdx = blockCommentEndIdx = -1;
+                        }
+                    }
+                }
+                else if (i >= blockCommentEndIdx)
+                {
+                    blockCommentStartIdx = blockCommentEndIdx = -1;
+                }
+
+                if (blockCommentStartIdx == -1 &&  lineCommentStartIdx == -1)
+                {
+                    lineCommentStartIdx = text.IndexOf("/" + "/", i);
+                    if (lineCommentStartIdx > -1)
+                    {
+                        lineCommentEndIdx = text.IndexOf("\n", lineCommentStartIdx);
+                        if (lineCommentEndIdx > lineCommentStartIdx)
+                        {
+                            lineCommentEndIdx += 2;
+                        }
+                        else
+                        {
+                            lineCommentStartIdx = lineCommentEndIdx = -1;
+                        }
+                    }
+                }
+                else if (i >= lineCommentEndIdx)
+                {
+                    lineCommentStartIdx = lineCommentEndIdx = -1;
+                }
+
+                if ((i >= blockCommentStartIdx && i <= blockCommentEndIdx)
+                    || (i >= lineCommentStartIdx && i <= lineCommentEndIdx))
+                {
+                    commentChars = char.IsLetterOrDigit(text[i]) ? commentChars + 1 : commentChars;
+                }
+                else
+                {
+                    codeChars = char.IsLetterOrDigit(text[i]) ? codeChars + 1 : codeChars;
+                }
+            }
+        }
+    }
+}
---
+++ FileWizard.ConApp/Program.cs
@@ -0,0 +1,241 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 FileWizard
+*                 This program is an exercise in file handling. 
+*                 The operations of copying a file, writing a 
+*                 file and reading a file should be practiced.
+*----------------------------------------------------------
+*/
+#nullable disable
+using System.Text;
+
+namespace FileWizard.ConApp
+{
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
+    internal class Program
+    {
+        static readonly ConsoleColor DefaultColor = Console.ForegroundColor;
+        const string LOGO_FILE = "logo.txt";
+
+        /// <summary>
+        /// The main method of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            string fileName;
+            string logo = File.ReadAllText(LOGO_FILE, Encoding.Default);
+
+            Console.WriteLine(logo);
+            fileName = GetFileName();
+            CreateBackup(fileName);
+
+            bool finished;
+
+            do
+            {
+                finished = PerformOperation(ref fileName);
+            } while (finished == false);
+
+            Console.WriteLine("Vielen Dank, dass ich Dir helfen durfte!");
+            Thread.Sleep(3000);
+        }
+
+        /// <summary>
+        /// Prompts the user to enter a file name and validates if the file exists.
+        /// </summary>
+        /// <returns>The name of the existing file entered by the user.</returns>
+        private static string GetFileName()
+        {
+            string result = string.Empty;
+            string input;
+            do
+            {
+                Console.Write("Welche Datei soll ich öffnen?: ");
+                input = Console.ReadLine();
+                if (File.Exists(input))
+                {
+                    result = input;
+                }
+                else
+                {
+                    Console.WriteLine("Diese Datei existiert nicht!");
+                }
+            } while (string.IsNullOrEmpty(result));
+
+            return result;
+        }
+
+        /// <summary>
+        /// Performs the selected operation based on user input.
+        /// </summary>
+        /// <param name="fileName">The name of the file to perform the operation on.</param>
+        /// <returns>True if the user selected to end the program, otherwise false.</returns>
+        private static bool PerformOperation(ref string fileName)
+        {
+            string input;
+
+//            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Was kann ich für dich tun?");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"FileName: {fileName}");
+            Console.ForegroundColor = DefaultColor;
+
+            Console.WriteLine();
+            Console.WriteLine("(1) Datei am Bildschirm ausgeben");
+            Console.WriteLine("(2) Zeilennummern hinzufügen");
+            Console.WriteLine("(3) Zeilen reversieren");
+            Console.WriteLine("(4) Zeichenketten ersetzen");
+            Console.WriteLine("(5) Neue Datei einlesen");
+            Console.WriteLine("(0) Ende");
+            Console.WriteLine();
+            Console.Write("Deine Wahl: ");
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    PrintFile(fileName);
+                    break;
+                case "2":
+                    AddLineNumbers(fileName);
+                    break;
+                case "3":
+                    ReverseLines(fileName);
+                    break;
+                case "4":
+                    ReplaceCharacters(fileName);
+                    break;
+                case "5":
+                    fileName = GetFileName();
+                    CreateBackup(fileName);
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.WriteLine("Ungültige Eingabe!");
+                    break;
+            }
+            return input == "0";
+        }
+
+        /// <summary>
+        /// Creates a backup of the specified file.
+        /// </summary>
+        /// <param name="fileName">The name of the file to create a backup for.</param>
+        private static void CreateBackup(string fileName)
+        {
+            int counter = 1;
+            string backupExtension = ".bak";
+            string backupFileName = $"{fileName}{backupExtension}";
+
+            while (File.Exists(backupFileName))
+            {
+                backupFileName = $"{fileName}{backupExtension}{counter}";
+                counter++;
+            }
+
+            if (File.Exists(fileName))
+            {
+                File.Copy(fileName, backupFileName);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Die Sicherungskopie '{backupFileName}' wurde erstellt.");
+                Console.ForegroundColor = DefaultColor;
+            }
+            else
+            {
+                Console.WriteLine($"Die Datei '{fileName}' existiert nicht!");
+            }
+        }
+
+        /// <summary>
+        /// Reverses the lines in a file.
+        /// </summary>
+        /// <param name="fileName">The name of the file to reverse the lines in.</param>
+        private static void ReverseLines(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                Array.Reverse(lines);
+                File.WriteAllLines(fileName, lines);
+            }
+            else
+            {
+                Console.WriteLine($"Die Datei '{fileName}' existiert nicht!");
+            }
+        }
+
+        /// <summary>
+        /// Adds line numbers to a file.
+        /// </summary>
+        /// <param name="fileName">The name of the file to add line numbers to.</param>
+        private static void AddLineNumbers(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                List<string> numberedLines = new List<string>();
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    numberedLines.Add($"{i + 1}: {lines[i]}");
+                }
+                File.WriteAllLines(fileName, numberedLines);
+            }
+            else
+            {
+                Console.WriteLine($"Die Datei '{fileName}' existiert nicht!");
+            }
+        }
+
+        /// <summary>
+        /// Replaces a specified string with a new string in the content of a file.
+        /// </summary>
+        /// <param name="fileName">The path of the file to modify.</param>
+        private static void ReplaceCharacters(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string content = File.ReadAllText(fileName);
+                Console.Write("Welche Zeichenkette soll ersetzt werden?:       ");
+                string oldString = Console.ReadLine();
+                Console.Write("Durch welche Zeichenkette soll ersetzt werden?: ");
+                string newString = Console.ReadLine();
+                content = content.Replace(oldString, newString);
+                File.WriteAllText(fileName, content);
+            }
+            else
+            {
+                Console.WriteLine($"Die Datei '{fileName}' existiert nicht!");
+            }
+        }
+
+        /// <summary>
+        /// Prints the contents of a file to the console.
+        /// </summary>
+        /// <param name="fileName">The name of the file to print.</param>
+        private static void PrintFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Die Datei '{fileName}' existiert nicht!");
+            }
+        }
+    }
+}
