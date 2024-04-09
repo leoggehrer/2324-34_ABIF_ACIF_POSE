8dfc92eb885b9ffd7858afe7f2eef12f76cd5beb
leoggehrer <g.gehrer@htl-leonding.ac.at>
2024-04-09 22:11:17 +0200

    ├änderungen in 2324-34_ABIF_ACIF_POSE committen

---
+++ UPNCalculator.ConApp/Program.cs
@@ -0,0 +1,98 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 StringSplit
+*                 A program that reads any text and another 
+*                 character “split” from the user. Then 
+*                 split the entered text according to each 
+*                 occurrence of the “Split” character 
+*                 in the text.
+*----------------------------------------------------------
+*/
+#nullable disable
+namespace UPNCalculator.ConApp
+{
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point for the application.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            string input, splitText;
+
+            Console.WriteLine("********************");
+            Console.WriteLine("* UPNCalculator    *");
+            Console.WriteLine("********************");
+            Console.WriteLine();
+
+            // Eingabe (E)
+            do
+            {
+                Console.Write("Rechenoperation [Enter...Exit]:  ");
+                input = Console.ReadLine();
+                if (input != string.Empty)
+                {
+                    // Verarbeitung (V)
+                    double result = Parse(input);
+
+                    // Ausgabe (A)
+                    Console.WriteLine($"{input} = {result}");
+                }
+            } while (input != string.Empty);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit: ");
+            Console.ReadKey();
+        }
+
+        private static double Parse(string input)
+        {
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Stack stack= new Stack();
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i] == "+")
+                {
+                    double a = stack.Pop();
+                    double b = stack.Pop();
+
+                    stack.Push(a + b);
+                }
+                else if (parts[i] == "-")
+                {
+                    double a = stack.Pop();
+                    double b = stack.Pop();
+
+                    stack.Push(a - b);
+                }
+                else if (parts[i] == "*")
+                {
+                    double a = stack.Pop();
+                    double b = stack.Pop();
+
+                    stack.Push(a * b);
+                }
+                else if (parts[i] == "/")
+                {
+                    double a = stack.Pop();
+                    double b = stack.Pop();
+
+                    stack.Push(a / b);
+                }
+                else if (double.TryParse(parts[i], out double opd))
+                {
+                    stack.Push(opd);
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
---
+++ UPNCalculator.ConApp/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UPNCalculator.ConApp
+{
+    public class Stack
+    {
+        #region nested class
+        private class Element
+        {
+            public Element(double data, Element? next)
+            {
+                Data = data;
+                Next = next;
+            }
+            public double Data { get; set; }
+            public Element? Next { get; set; }
+        }
+        #endregion  nested class
+
+        #region  fields
+        private Element? head = null;
+        #endregion fields
+
+        #region  properties
+        public bool IsEmpty
+        {
+            get
+            {
+                return head == null;
+            }
+        }
+        #endregion properties
+
+        #region  methods
+        public void Push(double data)
+        {
+            head = new Element(data, head);
+        }
+        public double Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty!");
+            }
+            
+            Element tmp = head!;
+
+            head = tmp.Next;
+            return tmp.Data;
+        }
+        #endregion methods
+    }
+}
+
