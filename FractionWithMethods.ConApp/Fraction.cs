04552085a00f9b9bbf9f872313f4120563e9aa3f
ggehrer <g.gehrer@hotmail.com>
2024-05-14 15:44:59 +0200

    Added Fraction with methods and operators.

---
+++ FractionWithMethods.ConApp/Fraction.cs
@@ -0,0 +1,226 @@
+﻿namespace FractionWithMethods.ConApp
+{
+    /// <summary>
+    /// Represents a fraction with a numerator and a denominator.
+    /// </summary>
+    public class Fraction
+    {
+        #region fields
+        private int _nominator = 0;
+        private int _denominator = 1;
+        #endregion fields
+
+        #region properties
+        /// <summary>
+        /// Gets or sets the denominator of the fraction.
+        /// </summary>
+        public int Denominator
+        {
+            get { return _denominator; }
+            set { _denominator = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the numerator of the fraction.
+        /// </summary>
+        public int Nominator
+        {
+            get { return _nominator; }
+            set { _nominator = value; }
+        }
+        #endregion properties
+        #region constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fraction"/> class with default values.
+        /// </summary>
+        public Fraction()
+        {
+            _nominator = 0;
+            _denominator = 1;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fraction"/> class with the specified numerator and denominator.
+        /// </summary>
+        /// <param name="a">The numerator of the fraction.</param>
+        /// <param name="b">The denominator of the fraction.</param>
+        public Fraction(int a, int b)
+        {
+            _nominator = a != 0 ? a : 0;
+            _denominator = b > 0 ? b : 1;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fraction"/> class with the specified numerator and a denominator of 1.
+        /// </summary>
+        /// <param name="a">The numerator of the fraction.</param>
+        public Fraction(int a)
+        {
+            _nominator = a != 0 ? a : 0;
+            _denominator = 1;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Fraction"/> class with the values of another fraction.
+        /// </summary>
+        /// <param name="a">The fraction to copy the values from.</param>
+        public Fraction(Fraction a)
+        {
+            _nominator = a._nominator;
+            _denominator = a._denominator;
+        }
+        #endregion constructors
+
+        #region methods
+        /// <summary>
+        /// Calculates and returns the decimal value of the fraction.
+        /// </summary>
+        /// <returns>The decimal value of the fraction.</returns>
+        public float GetValue()
+        {
+            return (float)Nominator / Denominator;
+        }
+
+        /// <summary>
+        /// Adds two fractions and returns the result.
+        /// </summary>
+        /// <param name="a">The first fraction.</param>
+        /// <param name="b">The second fraction.</param>
+        /// <returns>The sum of the two fractions.</returns>
+        public static Fraction Addition(Fraction a, Fraction b)
+        {
+            Fraction result;
+
+            if (a.Denominator == b.Denominator)
+                result = new Fraction(a.Nominator + b.Nominator, a.Denominator);
+            else
+                result = new Fraction((a.Nominator * b.Denominator + b.Nominator * a.Denominator), (a.Denominator * b.Denominator));
+            
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+
+        /// <summary>
+        /// Subtracts one fraction from another and returns the result.
+        /// </summary>
+        /// <param name="a">The first fraction.</param>
+        /// <param name="b">The second fraction.</param>
+        /// <returns>The difference between the two fractions.</returns>
+        public static Fraction Subtraction(Fraction a, Fraction b)
+        {
+            Fraction result;
+
+            if (a.Denominator == b.Denominator)
+                result = new Fraction(a.Nominator - b.Nominator, a.Denominator);
+            else
+                result = new Fraction((a.Nominator * b.Denominator - b.Nominator * a.Denominator), (a.Denominator * b.Denominator));
+
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplies two fractions and returns the result.
+        /// </summary>
+        /// <param name="a">The first fraction.</param>
+        /// <param name="b">The second fraction.</param>
+        /// <returns>The product of the two fractions.</returns>
+        public static Fraction Multiplication(Fraction a, Fraction b)
+        {
+            Fraction result = new Fraction(a.Nominator * b.Nominator, a.Denominator * b.Denominator);
+
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+
+        /// <summary>
+        /// Divides one fraction by another and returns the result.
+        /// </summary>
+        /// <param name="a">The first fraction.</param>
+        /// <param name="b">The second fraction.</param>
+        /// <returns>The quotient of the two fractions.</returns>
+        public static Fraction Division(Fraction a, Fraction b)
+        {
+            Fraction result = new Fraction(a.Nominator * b.Denominator, a.Denominator * b.Nominator);
+
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+
+        /// <summary>
+        /// Adds an integer to a fraction and returns the result.
+        /// </summary>
+        /// <param name="a">The fraction.</param>
+        /// <param name="b">The integer.</param>
+        /// <returns>The sum of the fraction and the integer.</returns>
+        public static Fraction Addition(Fraction a, int b)
+        {
+            Fraction result = Addition(a, new Fraction(b));
+
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+
+        /// <summary>
+        /// Subtracts an integer from a fraction and returns the result.
+        /// </summary>
+        /// <param name="a">The fraction.</param>
+        /// <param name="b">The integer.</param>
+        /// <returns>The difference between the fraction and the integer.</returns>
+        public static Fraction Subtraction(Fraction a, int b)
+        {
+            Fraction result = Subtraction(a, new Fraction(b));
+
+            int cgd = CGD(result.Nominator, result.Denominator);
+
+            result.Denominator /= cgd;
+            result.Nominator /= cgd;
+            return result;
+        }
+        /// <summary>
+        /// Calculates the greatest common divisor (GCD) of two integers using the recursive method.
+        /// </summary>
+        /// <param name="a">The first integer.</param>
+        /// <param name="b">The second integer.</param>
+        /// <returns>The greatest common divisor of the two integers.</returns>
+        public static int CGD(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return CGD(b, a % b);
+            }
+        }
+        #endregion methods
+
+        #region overrides
+        /// <summary>
+        /// Returns a string representation of the fraction.
+        /// </summary>
+        /// <returns>A string representation of the fraction.</returns>
+        public override string ToString()
+        {
+            return $"{Nominator}/{Denominator}";
+        }
+        #endregion overrides
+    }
+}
+
---
+++ FractionWithMethods.ConApp/Program.cs
@@ -0,0 +1,82 @@
+﻿/*----------------------------------------------------------
+*                 HTBLA-Leonding / Class: 3ABIF/3ACIF
+*----------------------------------------------------------
+*                 Hermann Mustermann
+*----------------------------------------------------------
+*                 Fraction with methods
+*----------------------------------------------------------
+*/
+#nullable disable
+namespace FractionWithMethods.ConApp
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            // Eingabe (E)
+            Console.WriteLine("***************************************");
+            Console.WriteLine("* Fraction with methods               *");
+            Console.WriteLine("***************************************");
+
+            Fraction a = new Fraction();        // Zähler:0, Nenner: 1
+            Fraction b = new Fraction(6, 4);    // Zähler:6, Nenner: 4
+            Fraction c = new Fraction(5);       // Zähler:5, Nenner: 1
+            Fraction d = new Fraction(b);       // Zähler:6, Nenner: 4
+            Fraction e = new Fraction(3, 0);    // Zähler:3, Nenner: 1, d.h. Nenner 0 ist nicht erlaubt
+            Fraction f, g, h, i, j, k, l, m, n, o, p, q, s;
+
+            
+            Console.WriteLine($"Bruch a: {a} ... Wert von a: {a.GetValue()}");
+            Console.WriteLine($"Bruch b: {b} ... Wert von a: {b.GetValue()}");
+            Console.WriteLine($"Bruch c: {c} ... Wert von a: {c.GetValue()}");
+            Console.WriteLine($"Bruch d: {d} ... Wert von a: {d.GetValue()}");
+            Console.WriteLine($"Bruch e: {e} ... Wert von a: {e.GetValue()}");
+
+
+            s = Fraction.Addition(a, 3);
+            f = Fraction.Addition(b, d);
+            g = Fraction.Addition(c, e);
+            h = Fraction.Addition(f, g);
+            Console.WriteLine();
+            Console.WriteLine($"Addition von Bruch {b} und {d} = {f.ToString()} ... Wert vom Bruch = {f.GetValue()}");
+            Console.WriteLine($"Addition von Bruch {c} und {e} = {g.ToString()} ... Wert vom Bruch = {g.GetValue()}");
+            Console.WriteLine($"Addition von Bruch {f} und {g} = {h.ToString()} ... Wert vom Bruch = {h.GetValue()}");
+            Console.WriteLine($"Addition von Bruch {a} und {3} = {s.ToString()} ... Wert vom Bruch = {s.GetValue()}");
+
+
+            i = Fraction.Subtraction(b, d);
+            j = Fraction.Subtraction(c, e);
+            k = Fraction.Subtraction(f, g);
+            Console.WriteLine();
+            Console.WriteLine($"Subtraktion von Bruch {b} und {d} = {i.ToString()} ... Wert vom Bruch = {i.GetValue()}");
+            Console.WriteLine($"Subtraktion von Bruch {c} und {e} = {j.ToString()} ... Wert vom Bruch = {j.GetValue()}");
+            Console.WriteLine($"Subtraktion von Bruch {f} und {g} = {k.ToString()} ... Wert vom Bruch = {k.GetValue()}");
+
+
+            l = Fraction.Division(b, d);
+            m = Fraction.Division(c, e);
+            n = Fraction.Division(f, g);
+            Console.WriteLine();
+            Console.WriteLine($"Division von Bruch {b} und {d} = {l.ToString()} ... Wert vom Bruch = {l.GetValue()}");
+            Console.WriteLine($"Division von Bruch {c} und {e} = {m.ToString()} ... Wert vom Bruch = {m.GetValue()}");
+            Console.WriteLine($"Division von Bruch {f} und {g} = {n.ToString()} ... Wert vom Bruch = {n.GetValue()}");
+
+
+            o = Fraction.Multiplication(b, d);
+            p = Fraction.Multiplication(c, e);
+            q = Fraction.Multiplication(f, g);
+            Console.WriteLine();
+            Console.WriteLine($"Multiplikation von Bruch {b} und {d} = {o.ToString()} ... Wert vom Bruch = {o.GetValue()}");
+            Console.WriteLine($"Multiplikation von Bruch {c} und {e} = {p.ToString()} ... Wert vom Bruch = {p.GetValue()}");
+            Console.WriteLine($"Multiplikation von Bruch {f} und {g} = {q.ToString()} ... Wert vom Bruch = {q.GetValue()}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to exit: ");
+            Console.ReadLine();
+        }
+    }
+}
