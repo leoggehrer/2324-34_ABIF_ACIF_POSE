82b8290dc9627cccf70cc9b3148b4838d0468678
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2024-05-23 23:01:29 +0200

    Generate sort diagrams.

--- SortingAlgorithms.ConApp/Sort.cs
+++ SortingAlgorithms.ConApp/Sort.cs
@@ -8,13 +8,13 @@
         /// <param name="array">The array to be sorted.</param>
         public static void BruteForceSort(int[] array)
         {
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 0; j < array.Length - 1; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    if (array[j] > array[j + 1])
+                    if (array[i] < array[j])
                     {
-                        Swap(ref array[j], ref array[j + 1]);
+                        Swap(ref array[i], ref array[j]);
                     }
                 }
             }
